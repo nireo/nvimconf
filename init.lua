5b89ccd8ec14c2fb4efe07420476aed03bab7a9b
nireo <eemil.lehtikangas1@gmail.com>
2022-11-05 16:14:49 +0200

    first commit

---
+++ init.lua
@@ -0,0 +1,292 @@
+-- Install packer
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local is_bootstrap = false
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  is_bootstrap = true
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+  vim.cmd [[packadd packer.nvim]]
+end
+
+require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'                                                         -- Package manager
+  use 'nvim-lualine/lualine.nvim'                                                      -- Fancier statusline
+
+  -- Themes
+  use 'AlessandroYorba/Alduin'
+  use 'danishprakash/vim-yami'
+  use 'bluz71/vim-nightfly-guicolors'
+  use 'nanotech/jellybeans.vim'
+
+  -- Surround
+  use 'tpope/vim-surround'
+
+  -- Commenting
+  use 'numToStr/Comment.nvim'
+
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/nvim-cmp'
+
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+  use 'onsails/lspkind.nvim'
+
+  use {
+   'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
+    end
+  }
+
+  use 'glepnir/lspsaga.nvim'
+
+  -- Fuzzy Finder (files, lsp, etc)
+  use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+
+  if is_bootstrap then
+    require('packer').sync()
+  end
+end)
+
+-- When we are bootstrapping a configuration, it doesn't
+-- make sense to execute the rest of the init.lua.
+--
+-- You'll need to restart nvim, and then it will work.
+if is_bootstrap then
+  print '=================================='
+  print '    Plugins are being installed'
+  print '    Wait until Packer completes,'
+  print '       then restart nvim'
+  print '=================================='
+  return
+end
+
+-- Automatically source and re-compile packer whenever you save this init.lua
+local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePost', {
+  command = 'source <afile> | PackerCompile',
+  group = packer_group,
+  pattern = vim.fn.expand '$MYVIMRC',
+})
+
+-- [[ Setting options ]]
+-- See `:help vim.o`
+
+-- Set highlight on search
+vim.o.hlsearch = false
+
+-- Make line numbers default
+vim.o.relativenumber = true
+vim.o.number = true
+
+-- Backspace
+vim.o.backspace = "indent,eol,start"
+
+-- Enable mouse mode
+vim.o.mouse = 'a'
+
+-- Indentation changes
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.expandtab = true
+vim.o.autoindent = true
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Enable usage of system clipboard.
+vim.o.clipboard = 'unnamedplus'
+
+vim.o.cursorline = true
+
+-- Decrease update time
+vim.o.updatetime = 100
+vim.wo.signcolumn = 'yes'
+
+-- Set colorscheme
+vim.o.termguicolors = true
+vim.cmd [[colorscheme jellybeans]]
+
+-- Window splits
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+-- [[ Basic Keymaps ]]
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+vim.keymap.set('i', '<C-j>', '<ESC>')
+vim.keymap.set('i', '<C-f>', '<ESC>')
+vim.keymap.set('n', '<leader>s', ':w!<CR>')
+
+vim.keymap.set('n', '<leader>p', '<cmd>Telescope find_files<cr>')
+vim.keymap.set('n', '<leader>l', '<cmd>Telescope buffers<cr>')
+
+vim.keymap.set('n', '<leader>wc', ':close<cr>')
+vim.keymap.set('n', '<leader>sv', '<C-w>v')
+vim.keymap.set('n', '<leader>sh', '<C-w>s')
+
+vim.o.backup = false
+vim.o.scrolloff = 5
+vim.o.swapfile = false
+vim.o.fileencoding = 'utf-8'
+vim.o.showmode = false
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
+require('lualine').setup()
+require('Comment').setup()
+
+local telescope_setup, telescope = pcall(require, 'telescope')
+if not telescope_setup then
+  return
+end
+
+local actions_setup, actions = pcall(require, 'telescope.actions')
+if not actions_setup then
+  return
+end
+
+telescope.setup {
+  defaults = {
+    mappings = {
+      i = { 
+        ['<C-k>'] = actions.move_selection_previous,
+        ['<C-j>'] = actions.move_selection_next,
+      }
+    }
+  }
+}
+
+telescope.load_extension('fzf')
+
+vim.opt.completeopt = "menu,menuone,noselect"
+
+local cmp_setup, cmp = pcall(require, 'cmp')
+if not cmp_setup then
+  return
+end
+
+cmp.setup {
+  mapping = cmp.mapping.preset.insert({
+		["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+		["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+		["<C-e>"] = cmp.mapping.abort(), -- close completion window
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
+	}),
+
+	-- sources for autocompletion
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" }, -- lsp
+		{ name = "luasnip" }, -- snippets
+	  { name = "buffer" }, -- text within current buffer
+		{ name = "path" }, -- file system paths
+	}),
+}
+
+require('mason').setup()
+require('mason-lspconfig').setup {
+  ensure_installed = {
+    'clangd',
+    'gopls',
+  }
+}
+
+local lspconfig_status, lspconfig = pcall(require, 'lspconfig')
+if not lspconfig_status then
+  return
+end
+
+local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if not cmp_nvim_lsp_status then
+	return
+end
+
+local keymap = vim.keymap -- for conciseness
+
+-- enable keybinds only for when lsp server available
+local on_attach = function(client, bufnr)
+	-- keybind options
+	local opts = { noremap = true, silent = true, buffer = bufnr }
+
+	-- set keybinds
+	keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
+	-- keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
+	keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
+	keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
+	keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
+	keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
+	keymap.set("n", "<leader>d", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
+	keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
+	keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- jump to previous diagnostic in buffer
+	keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
+	keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
+  local capabilities = cmp_nvim_lsp.default_capabilities()
+end
+
+lspconfig["gopls"].setup {
+  capabilities = capabilities,
+  on_attach = on_attach,
+}
+
+lspconfig["clangd"].setup {
+  capabilities = capabilities,
+  on_attach = on_attach,
+}
+
+local saga_status, saga = pcall(require, "lspsaga")
+if not saga_status then
+	return
+end
+
+saga.init_lsp_saga({
+	-- keybinds for navigation in lspsaga window
+	move_in_saga = { prev = "<C-k>", next = "<C-j>" },
+	-- use enter to open file with finder
+	finder_action_keys = {
+		open = "<CR>",
+	},
+	-- use enter to open file with definition preview
+	definition_action_keys = {
+		edit = "<CR>",
+	},
+})
+
+require('lspkind')
+
