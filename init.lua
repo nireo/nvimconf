17e8fb08e76fd3f195b332e3050b6ce9e4b2db1e
nireo <eemil.lehtikangas1@gmail.com>
2023-10-23 01:15:35 +0300

    added hop.nvim

--- init.lua
+++ init.lua
@@ -109,6 +109,14 @@ local plugins = {
 		branch = "0.1.x",
 		dependencies = { "nvim-lua/plenary.nvim" },
 	},
+  {
+    "phaazon/hop.nvim",
+    branch = "v2",
+    config = function()
+      -- you can configure Hop the way you like here; see :h hop-config
+      require'hop'.setup { }
+    end
+  },
 	performance = {
 		rtp = {
 			disabled_plugins = {
@@ -612,3 +620,18 @@ require("todo-comments").setup({})
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 vim.g.rust_recommended_style = 0
 vim.cmd("colorscheme neobones")
+
+local hop = require('hop')
+local directions = require('hop.hint').HintDirection
+vim.keymap.set('', 'f', function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+end, {remap=true})
+vim.keymap.set('', 'F', function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+end, {remap=true})
+vim.keymap.set('', 't', function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+end, {remap=true})
+vim.keymap.set('', 'T', function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+end, {remap=true})
