46dbf79e6fbc9d9d6bd9c955246702cd60fa963d
nireo <eemil.lehtikangas1@gmail.com>
2024-02-22 23:54:32 +0200

    removed hop

--- init.lua
+++ init.lua
@@ -109,14 +109,6 @@ local plugins = {
 		branch = "0.1.x",
 		dependencies = { "nvim-lua/plenary.nvim" },
 	},
-	{
-		"phaazon/hop.nvim",
-		branch = "v2",
-		config = function()
-			-- you can configure Hop the way you like here; see :h hop-config
-			require("hop").setup({})
-		end,
-	},
 	performance = {
 		rtp = {
 			disabled_plugins = {
@@ -578,18 +570,3 @@ require("colorizer").setup()
 require("todo-comments").setup({})
 
 vim.cmd("colorscheme adwaita")
-
-local hop = require("hop")
-local directions = require("hop.hint").HintDirection
-vim.keymap.set("", "f", function()
-	hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
-end, { remap = true })
-vim.keymap.set("", "F", function()
-	hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
-end, { remap = true })
-vim.keymap.set("", "t", function()
-	hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
-end, { remap = true })
-vim.keymap.set("", "T", function()
-	hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
-end, { remap = true })
