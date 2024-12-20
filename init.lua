e3ae08f49a8fefde177ad375c22cf6801654045e
nireo <eemil.lehtikangas1@gmail.com>
2024-12-20 19:31:04 +0200

    added nice keybinding

--- init.lua
+++ init.lua
@@ -32,10 +32,13 @@ local plugins = {
 	{
 		"saghen/blink.cmp",
 		dependencies = "rafamadriz/friendly-snippets",
-
 		version = "v0.*",
 		opts = {
-			keymap = { preset = "default" },
+			keymap = {
+				preset = "enter",
+				["<C-k>"] = { "select_prev", "fallback" },
+				["<C-j>"] = { "select_next", "fallback" },
+			},
 			appearance = {
 				use_nvim_cmp_as_default = true,
 				nerd_font_variant = "mono",
