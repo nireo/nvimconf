a7d4a509d922c57d68a86c46649be3c3cc9b178f
nireo <eemil.lehtikangas1@gmail.com>
2025-03-20 19:27:10 +0200

    add grug-far

--- init.lua
+++ init.lua
@@ -565,6 +565,37 @@ local plugins = {
 				desc = "Goto T[y]pe Definition",
 			},
 		},
+		{
+			"MagicDuck/grug-far.nvim",
+			config = function()
+				require("grug-far").setup({})
+
+				vim.api.nvim_create_autocmd("FileType", {
+					pattern = "grug-far",
+					callback = function()
+						-- Map <Esc> to quit after ensuring we're in normal mode
+						vim.keymap.set({ "i", "n" }, "<Esc>", "<Cmd>stopinsert | bd!<CR>", { buffer = true })
+					end,
+				})
+			end,
+			keys = {
+				{
+					"<leader>sr",
+					function()
+						local grug = require("grug-far")
+						local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+						grug.open({
+							transient = true,
+							prefills = {
+								filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+							},
+						})
+					end,
+					mode = { "n", "v" },
+					desc = "Search and Replace",
+				},
+			},
+		},
 	},
 	performance = {
 		rtp = {
